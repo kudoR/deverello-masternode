1c7bfc2f6c09189cbfaf8d19554d42285690266a
Johannes Hinkov <johannes.hinkov@gmail.com>
2020-04-19 19:02:52 +0200

    add config for basic auth user and pw

--- src/main/java/de/nld/deverello/masternode/controller/SurveyController.java
+++ src/main/java/de/nld/deverello/masternode/controller/SurveyController.java
@@ -3,6 +3,8 @@ package de.nld.deverello.masternode.controller;
 import de.nld.deverello.masternode.model.Survey;
 import de.nld.deverello.masternode.service.SurveyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +19,12 @@ public class SurveyController {
     @Autowired
     private SurveyService service;
 
-    @PostMapping
-    public Survey createSurvey(@RequestBody Survey survey, HttpServletRequest request) {
+    @PostMapping(produces = "application/json")
+    public ResponseEntity<?> createSurvey(@RequestBody Survey survey, HttpServletRequest request) {
         String remoteAddr = request.getRemoteAddr();
         survey.setRemoteAddr(remoteAddr);
-        return service.createSurvey(survey);
+        Survey createdSurvey = service.createSurvey(survey);
+        return new ResponseEntity<>(createdSurvey, HttpStatus.CREATED);
     }
 
 }
--- src/main/java/de/nld/deverello/masternode/security/CustomWebSecurityConfigurerAdapter.java
+++ src/main/java/de/nld/deverello/masternode/security/CustomWebSecurityConfigurerAdapter.java
@@ -11,6 +11,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -29,17 +34,33 @@ public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAda
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser(username).password(passwordEncoder().encode(password))
-                .authorities("ROLE_USER");
+                .roles("USER");
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http
+                .cors()
+                .and().csrf()
+                    .disable()
+                .authorizeRequests()
                 .antMatchers("/securityNone").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic()
-                .authenticationEntryPoint(authenticationEntryPoint);
+                .anyRequest()
+                    .authenticated()
+                .and().httpBasic()
+                    .authenticationEntryPoint(authenticationEntryPoint);
 
         http.addFilterAfter(new CustomFilter(),
                 BasicAuthenticationFilter.class);
